e8766db5ef5e54968b0d3dec86c0f10ecad1016e
Matheus Melo <matheusmelo180203@gmail.com>
2024-05-13 16:03:04 -0300

    primeiro commit

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ContaTerminal {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        
+
+    }
+
+
+}
